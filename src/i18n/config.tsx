8b30a7c90d85dd2826660da15b2aeefa2ad66b78
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2024-04-03 22:43:15 +0200

    Import languages statically

--- src/i18n/config.tsx
+++ src/i18n/config.tsx
@@ -2,40 +2,38 @@ import i18next, { InitOptions } from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import locales from "./locales.json";
+import enTranslations from "./locales/en-US.json";
+import deTranslations from "./locales/de-DE.json";
+import esTranslations from "./locales/es-ES.json";
+import frTranslations from "./locales/fr-FR.json";
+import csTranslations from "./locales/cs-CZ.json";
+import nlTranslations from "./locales/nl-NL.json";
+import elTranslations from "./locales/el-GR.json";
+import zhCnTranslations from "./locales/zh-CN.json";
+import zhTwTranslations from "./locales/zh-TW.json";
 
 const debug = Boolean(new URLSearchParams(window.location.search).get("debug"));
 
-const resources: InitOptions["resources"] = {};
-
-const data = import.meta.glob("./locales/*.json");
-
-for (const path in data) {
-  const code = path.replace(/^.*[\\/]/, "").replace(/\..*$/, "");
-  if (!locales.some(e => e.includes(code))) {
-    continue;
-  }
-  const short = code.replace(/-.*$/, "");
-  const main = locales.filter(l => l.indexOf(short) === 0).length === 1;
-
-  data[path]().then(mod => {
-    const translation = JSON.parse(JSON.stringify(mod));
-
-    if (!main) {
-      resources[code] = { translation: translation };
-    }
-    resources[short] = { translation: translation };
-  });
-}
+const resources: InitOptions["resources"] = {
+  en: { translation: enTranslations},
+  de: { translation: deTranslations},
+  es: { translation: esTranslations},
+  fr: { translation: frTranslations},
+  cs: { translation: csTranslations},
+  nl: { translation: nlTranslations},
+  el: { translation: elTranslations},
+  "zh-CN": { translation: zhCnTranslations},
+  "zh-TW": { translation: zhTwTranslations}
+};
 
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
-    resources,
     fallbackLng: ["en-US", "en"],
     nonExplicitSupportedLngs: true,
     debug: debug,
+    resources,
   });
 
 if (debug) {
